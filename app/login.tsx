64323b6374505d13ed6d886aa5b820859fb19969
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-08-06 12:49:24 +0530

    update

--- app/login.tsx
+++ app/login.tsx
@@ -1,8 +1,18 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Pressable, Text, TextInput, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Login() {
+
+    const { isLoggedIn, login, logout } = useAuth();
+
+    const handleLogin = () => {
+        console.log("clicked login button");
+        
+        // check credintial 
+        login()
+    }
   return (
     <View className="flex-1 items-center justify-center px-6 bg-white">
       <Text className="text-2xl font-bold mb-6">Login</Text>
@@ -17,7 +27,12 @@ export default function Login() {
         className="w-full border border-gray-300 rounded-md p-3 mb-4"
       />
 
-      <TouchableOpacity className="bg-blue-500 w-full p-3 rounded-md mb-4">
+      {/* <Pressable></Pressable> as like touchable opacity */}
+
+      <TouchableOpacity
+        className="bg-blue-500 w-full p-3 rounded-md mb-4 "
+        onPress={handleLogin}
+      >
         <Text className="text-white text-center font-semibold">Login</Text>
       </TouchableOpacity>
 

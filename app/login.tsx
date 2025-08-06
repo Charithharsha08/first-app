70bee15ba1c759b6c8ae77a77d52d0bf49d62ba7
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-08-06 13:10:28 +0530

    check credintial

--- app/login.tsx
+++ app/login.tsx
@@ -1,17 +1,41 @@
 import { Pressable, Text, TextInput, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { useAuth } from "@/context/AuthContext";
 
 export default function Login() {
 
+     const [Username, setUserName] = React.useState("");
+     const [Password, setPassword] = React.useState("");
+
     const { isLoggedIn, login, logout } = useAuth();
 
+    const router = useRouter();
+
     const handleLogin = () => {
-        console.log("clicked login button");
-        
-        // check credintial 
-        login()
+        if (Username === "admin" && Password === "admin") {
+          console.log("Login successful");
+          // Navigate to the profile page or perform any other action
+          router.replace("/"); // Use replace to avoid going back to login on back press
+          // or you can use router.push("/profile") if you want to keep the login in the history stack
+          // This will navigate to the profile page
+          setUserName(""); // Clear the username field
+          setPassword(""); // Clear the password field
+          login();
+          return; // Stop further execution
+        } else {
+            console.log("Login failed");
+            // Show an error message or handle the failed login
+            alert("Invalid credentials. Please try again.");
+            setUserName("");
+            setPassword("");
+            // Optionally, you can reset the form or redirect to another page
+            // router.push("/"); // Redirect to home or another page
+            // return; // If you want to stop further execution
+            // or you can just return to stop further execution
+            //  
+            return;
+        }  
     }
   return (
     <View className="flex-1 items-center justify-center px-6 bg-white">
@@ -20,11 +44,15 @@ export default function Login() {
       <TextInput
         placeholder="Username"
         className="w-full border border-gray-300 rounded-md p-3 mb-4"
+        value={Username}
+        onChangeText={setUserName}
       />
       <TextInput
         placeholder="Password"
         secureTextEntry
         className="w-full border border-gray-300 rounded-md p-3 mb-4"
+        value={Password}
+        onChangeText={setPassword}
       />
 
       {/* <Pressable></Pressable> as like touchable opacity */}
